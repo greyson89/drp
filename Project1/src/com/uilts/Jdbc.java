71b3e015ac5e99f240bd33d9470a78208363e61a
Greyson <user@Greyson-NB>
2016-12-21 15:29:24 +0800

    3rd

--- Project1/src/com/uilts/Jdbc.java
+++ Project1/src/com/uilts/Jdbc.java
@@ -11,7 +11,7 @@ public class Jdbc {
 		String driver = "com.mysql.jdbc.Driver"; 
         String url = "jdbc:mysql://localhost:3306/?useSSL=false"; 
         String user = ""; 
-        String password = ""; 
+        String password = ""; //test
         Connection conn = null;
 
         try { 
